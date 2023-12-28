d85c36e5049b6555f8f2118bc07556ee3f774e7d
Rodrigo Rodrigues <rodribola.0886@gmail.com>
2023-12-28 21:49:26 +0000

    Feat: api-proc

---
+++ src/api/proc/converter.py
@@ -0,0 +1,20 @@
+import xmltodict
+import json
+import re
+
+def convert_xml_to_json(xml):    
+    data_dict = xmltodict.parse(xml)
+    json_data = json.dumps(data_dict)
+    return json_data
+
+def merge_xml_element(xml, element_name):
+    xml = xml.replace("No results found", "")
+    pattern = fr"<{element_name}>(.*?)</{element_name}>"
+    matches = re.findall(pattern, xml, re.DOTALL)
+    merged_element = f"<{element_name}>" + " ".join(matches) + f"</{element_name}>"
+    return merged_element
+
+def get_parent_element(xml):
+    pattern = r"<(.*?)>"
+    match = re.findall(pattern, xml)
+    return match[0]
